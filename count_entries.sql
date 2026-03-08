1ba89e392a5311bb61975e1569358ec8e7236bb8
arturossi <arturossi10@gmail.com>
2026-03-08 20:09:36 -0300

    Update resource allocation in config.yaml and remove obsolete count_entries.sql

--- count_entries.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    (SELECT COUNT(*) FROM complexes) AS N_Complexes,
-    (SELECT COUNT(*) FROM receptors) AS N_Receptors,
-    (SELECT COUNT(*) FROM ligands) AS N_Ligands,
-    (SELECT COUNT(DISTINCT `name`) FROM complexes) as Unique_Complexes;
